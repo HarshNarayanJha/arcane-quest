44c0779f7f11438a9bf22893c3532048b5a6d6de
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-11-20 00:20:24 +0530

    basic reflecting laser implementation

---
+++ Scripts/Entities/Objects/laser.gd
@@ -0,0 +1,59 @@
+extends StaticBody2D
+
+@export var sprite: Sprite2D
+@export var raycast: RayCast2D
+@export var line: Line2D
+@export var particles: GPUParticles2D
+
+@export var bounces := 5
+
+const MAX_LENGTH := 1000
+#var max_cast_to: Vector2
+
+#var lasers : Array[RayCast2D] = []
+
+func _ready() -> void:
+	pass
+	
+func _draw() -> void:
+	draw_circle(raycast.position, 10, Color.AQUAMARINE)
+	var normal_end_point = raycast.get_collision_point() + raycast.get_collision_normal() * 80.0
+	draw_line(to_local(raycast.get_collision_point()), to_local(normal_end_point), Color.GREEN, 10)
+
+func _process(delta: float) -> void:
+	queue_redraw()
+	
+	# clear the laser and add origin
+	line.clear_points()
+	line.add_point(Vector2.ZERO)
+	
+	raycast.global_position = line.global_position
+	raycast.target_position = transform.x.normalized() * MAX_LENGTH
+	#raycast.target_position = (get_local_mouse_position() - line.position).normalized() * MAX_LENGTH
+	raycast.force_raycast_update()
+	
+	for k in bounces:
+		if not raycast.is_colliding():
+			var pt = raycast.position + raycast.target_position
+			line.add_point(pt)
+			#particles.emitting = false
+			break
+			
+		var coll = raycast.get_collider()
+		var pt = raycast.get_collision_point()
+		
+		line.add_point(line.to_local(pt))
+		
+		#if not coll.is_mirrot():
+			#break
+		
+		var normal = raycast.get_collision_normal()
+		if normal == Vector2.ZERO:
+			break
+			
+		particles.global_position = pt
+		particles.emitting = true
+			
+		raycast.global_position = pt
+		raycast.target_position = raycast.target_position.bounce(normal.rotated(-global_rotation))
+		raycast.force_raycast_update()
